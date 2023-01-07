7986c578d879176b74d89a2451b4ebcbe8ed0187
JaehoonSim <18tomcat@gmail.com>
2023-01-07 16:44:10 +0900

    123

--- main.py
+++ main.py
@@ -2,41 +2,45 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-url = 'https://ridibooks.com/category/bestsellers/6050?page=1'
-req_header_dict = {
-    # 요청헤더 : 브라우저정보
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
-}
-res = requests.get(url, headers=req_header_dict)
-rost = []
-html = res.text
+url = 'https://api.ridibooks.com/v2/bestsellers?category_includes=6050&period=DAILY&limit=100'
 
-soup = BeautifulSoup(html, 'html.parser')
+res = requests.get(url)
 
-books = soup.select_one("#__NEXT_DATA__")
-books = books.text
+data = json.loads(res.text)
 
-a = json.loads(books)
+my_data = data['data']['items']
+
 rank = 1
-book_list = []
 
-book_list.append(a)
-
-with open('ridi_rf_top60.json', 'w', encoding='utf-8') as file:
-    json.dump(book_list, file, ensure_ascii=False, indent="\t")
-
-with open('ridi_rf_top60.json', 'rt', encoding='UTF8') as f:
-    # Load the JSON data into a Python dictionary
-    data = json.load(f)
-
-# Extract the list from the dictionary
-my_list = data[0]["props"]["pageProps"]["dehydratedState"]["queries"][2]["state"]["data"]
-# print(len(my_list))
-for list_item in my_list:
+for list_item in my_data:
     list_item['book']['rank'] = rank
     rank = rank + 1
     list_item['book']['link'] = "https://ridibooks.com/books/" + \
-        list_item['book']['bookId']
+        list_item['book']['book_id']
 
-with open('ridi_rf_top60.json', 'w', encoding='utf-8') as file:
-    json.dump(my_list, file, ensure_ascii=False, indent="\t")
+with open('ridi.json', 'w', encoding='utf-8') as file:
+    json.dump(my_data, file, ensure_ascii=False, indent="\t")
+
+# rank = 1
+# book_list = []
+
+# book_list.append(a)
+
+# with open('ridi_rf_top60.json', 'w', encoding='utf-8') as file:
+#     json.dump(book_list, file, ensure_ascii=False, indent="\t")
+
+# with open('ridi_rf_top60.json', 'rt', encoding='UTF8') as f:
+#     # Load the JSON data into a Python dictionary
+#     data = json.load(f)
+
+# # Extract the list from the dictionary
+# my_list = data[0]["props"]["pageProps"]["dehydratedState"]["queries"][2]["state"]["data"]
+# # print(len(my_list))
+# for list_item in my_list:
+#     list_item['book']['rank'] = rank
+#     rank = rank + 1
+#     list_item['book']['link'] = "https://ridibooks.com/books/" + \
+#         list_item['book']['bookId']
+
+# with open('ridi_rf_top60.json', 'w', encoding='utf-8') as file:
+#     json.dump(my_list, file, ensure_ascii=False, indent="\t")
---
+++ main_old.py
@@ -0,0 +1,42 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+url = 'https://ridibooks.com/category/bestsellers/6050?page=1'
+req_header_dict = {
+    # 요청헤더 : 브라우저정보
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
+}
+res = requests.get(url, headers=req_header_dict)
+rost = []
+html = res.text
+
+soup = BeautifulSoup(html, 'html.parser')
+
+books = soup.select_one("#__NEXT_DATA__")
+books = books.text
+
+a = json.loads(books)
+rank = 1
+book_list = []
+
+book_list.append(a)
+
+with open('ridi_rf_top60.json', 'w', encoding='utf-8') as file:
+    json.dump(book_list, file, ensure_ascii=False, indent="\t")
+
+with open('ridi_rf_top60.json', 'rt', encoding='UTF8') as f:
+    # Load the JSON data into a Python dictionary
+    data = json.load(f)
+
+# Extract the list from the dictionary
+my_list = data[0]["props"]["pageProps"]["dehydratedState"]["queries"][2]["state"]["data"]
+# print(len(my_list))
+for list_item in my_list:
+    list_item['book']['rank'] = rank
+    rank = rank + 1
+    list_item['book']['link'] = "https://ridibooks.com/books/" + \
+        list_item['book']['bookId']
+
+with open('ridi_rf_top60.json', 'w', encoding='utf-8') as file:
+    json.dump(my_list, file, ensure_ascii=False, indent="\t")
